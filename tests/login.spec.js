504ac5df100152875f85079b7daf048aee574532
Tejas Shinde <tshinde@anthology.com>
2025-04-04 16:44:27 +0530

    1st commit

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,6 +1,7 @@
 // login.spec.js
 const { test, expect } = require('@playwright/test');
 
+//1st test
 test('Open Harold Waste Demo Site', async ({ page }) => {
   await page.goto('https://demo.haroldwaste.com');
   await expect(page).toHaveTitle(/Jules.ai/);
